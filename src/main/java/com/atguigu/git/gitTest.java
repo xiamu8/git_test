43b229d02f241510bcb2ae2fd554c4e0d9cc52df
tina <1021625050@qq.com>
2022-06-05 16:29:41 +0800

    push commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -1,11 +1,5 @@
 package com.atguigu.git;
 
-/**
- * @Auther: zhaoss
- * @Date: 2022/6/5 - 06 - 05 - 10:51
- * @Description: com.atguigu.git
- * @version: 1.0
- */
 public class gitTest {
     //这是一个main方法，程序的入口
     public static void main(String[] args) {
@@ -15,6 +9,6 @@ public class gitTest {
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hello git5!");
-    }
-
+        System.out.println("push test!");
+         }
 }
