df88da0f79af467cedbe790f1bd9bd5dd7bb7062
tina <1021625050@qq.com>
2022-06-05 11:52:17 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -12,6 +12,7 @@ public class gitTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 
 }
