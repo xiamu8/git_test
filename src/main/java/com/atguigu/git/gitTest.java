52c506a843c52968fea991bb73b4e3869a1176db
tina <1021625050@qq.com>
2022-06-05 11:10:48 +0800

    second commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -10,6 +10,7 @@ public class gitTest {
     //这是一个main方法，程序的入口
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 
 }
