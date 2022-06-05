8f4a0b53adae612afad740c3362e25f7d22c9a7a
tina <1021625050@qq.com>
2022-06-05 11:02:44 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @Auther: zhaoss
+ * @Date: 2022/6/5 - 06 - 05 - 10:51
+ * @Description: com.atguigu.git
+ * @version: 1.0
+ */
+public class gitTest {
+    //这是一个main方法，程序的入口
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
